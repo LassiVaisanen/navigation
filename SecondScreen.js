930a5dc6c13e8b1e508e355ce66cdd996cd4e226
LassiVaisanen <t1vala02@students.oamk.fi>
2023-09-25 22:38:37 +0300

    Initial commit

---
+++ SecondScreen.js
@@ -0,0 +1,35 @@
+import React, { useLayoutEffect, useEffect } from "react";
+import { StyleSheet, Text, View, BackHandler } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'
+
+export default function SecondScreen({route, navigation}) {
+
+    useEffect(() => {
+        if (route.params?.message) {
+            alert(route.params.message);
+        }
+        BackHandler.addEventListener('hardwareBackPress', close);
+        return() => {
+            BackHandler.removeEventListener('hardwareBackPress', close);
+        }
+    }, [])
+
+    function close() {
+        navigation.goBack(null);
+        return true;
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerStyle: {
+                backgroundColor: 'f0f0f0'
+            }
+        })
+    }, [])
+}
+
+const styles = StyleSheet.create({
+    container: {
+      padding: 10,
+    },
+});
